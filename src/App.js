a4556f6a2cee8cc67eec077f407f0bb25e64b6d5
iulianstan <iulian1226@gmail.com>
2021-06-08 13:41:10 +0200

    src/App.js/ Import React, useState, UseEffect from 'react', axios from 'axios' and Loading.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+// Import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
+// Import axios from "axios"
+import axios from "axios";
+// Import Loading
+import Loading from "./components/Loading";
+
+// App function
 function App() {
   return <div className="App"></div>;
 }
 
+// Export App
 export default App;
