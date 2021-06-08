a81d9f13d0cb9dceb08baeaf3030450b0dba05c1
iulianstan <iulian1226@gmail.com>
2021-06-08 13:35:27 +0200

    public/Ajax-loader.gif/ Added loader gif

---
+++ src/components/Loading.js
@@ -0,0 +1,17 @@
+// Import React from "react"
+import React from "react";
+
+// Loading function
+const Loading = () => {
+  return (
+    <React.Fragment>
+      <h4>
+        <img scr="../public/Ajax-loader.gif" alt="Spinning loader" />
+      </h4>
+      <h6>Loading ...</h6>
+    </React.Fragment>
+  );
+};
+
+// Export Loading
+export default Loading;
