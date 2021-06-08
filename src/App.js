2f0e8267a6b16371ea1841b289174f40eb8a66dc
iulianstan <iulian1226@gmail.com>
2021-06-08 13:43:19 +0200

    src/App.js/ Added Loading as jsx element

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 // Import React, { useState, useEffect } from "react"
 import React, { useState, useEffect } from "react";
 // Import axios from "axios"
-import axios from "axios";
+//import axios from "axios";
 // Import Loading
 import Loading from "./components/Loading";
 
 // App function
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Loading />
+    </div>
+  );
 }
 
 // Export App
