2d26853a4d0c009af00d4b783fa514499e591158
iulianstan <iulian1226@gmail.com>
2021-06-08 15:42:08 +0200

    src/sass/layouts/_classes.scss/ Loader setup

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -5,13 +5,15 @@ import React from "react";
 const Loading = () => {
   return (
     <React.Fragment>
-      <h4>
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/d/de/Ajax-loader.gif"
-          alt="Ajax-loader.gif"
-        />
-      </h4>
-      <h6>Loading ...</h6>
+      <div className="loader">
+        <h4>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/d/de/Ajax-loader.gif"
+            alt="Ajax-loader.gif"
+          />
+        </h4>
+        <h6>Loading ...</h6>
+      </div>
     </React.Fragment>
   );
 };
